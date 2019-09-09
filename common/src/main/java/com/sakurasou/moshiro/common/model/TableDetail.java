391e104333b5f9e50c4eb3281083253077e318e3
libvirgo <sakurapetgirl@live.com>
2019-09-10 01:30:08 +0800

    修改Entity,Repository,Table名称,更好的语义化.

--- common/src/main/java/com/sakurasou/moshiro/common/model/TableDetail.java
+++ common/src/main/java/com/sakurasou/moshiro/common/model/TableDetail.java
@@ -5,18 +5,21 @@ import lombok.Data;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
-/*** 该类用来记录各表的主键,以进行主键的生成.
+/*** 该类用来记录各表的结构信息.
  * @author clytie
  * @version 0.0.1-SNAPSHOT
  * */
 
-@Entity
 @Data
-public class SequenceId {
+@Entity
+@Table(name = "table_detail")
+public class TableDetail {
     @Id
     @Column(name = "table_name", length = 100)
     String tableName;
-    @Column(name = "table_sequence",length = 100)
-    String tableSequence;
+    @Column(name = "table_id_sequence",length = 100)
+    String tableIdSequence;
 }
+
